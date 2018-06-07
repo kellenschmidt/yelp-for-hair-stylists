51b80932219c13ff7fae91df1d05bf47c0b989e3
danny-vo <dannyphvo@gmail.com>
2018-06-06 20:40:46 -0400

    Added ReviewList to StylistPage

--- yfhs/src/components/StylistPage/StylistPage.js
+++ yfhs/src/components/StylistPage/StylistPage.js
@@ -3,11 +3,47 @@ import PropTypes from 'prop-types';
 import Typography from '@material-ui/core/Typography';
 
 import './StylistPage.css';
+import ReviewList from '../ReviewList/ReviewList';
 
 class StylistPage extends Component {
   constructor(props) {
     super(props);
   }
+
+  reviews = [
+    {
+      title: "Super Great Haircut",
+      description: "Review Description Review Description Review Description Review Description Review Description Review Description Review Description Review Description Review Description Review Description Review Description Review Description Review Description",
+      overall_score: "1.8",
+      style: 0,
+      gender: 0,
+      age: "14"
+    },
+    {
+      title: "Super Great Haircut",
+      description: "Review Description Review Description Review Description Review Description Review Description Review Description Review Description Review Description Review Description Review Description Review Description Review Description",
+      overall_score: "2.8",
+      style: 1,
+      gender: 1,
+      age: "21"
+    },
+    {
+      title: "Super Great Haircut",
+      description: "Review Description Review Description Review Description Review Description Review Description Review Description Review Description Review Description Review Description Review Description Review Description Review Description Review Description Review Description Review Description",
+      overall_score: "3.8",
+      style: 2,
+      gender: 1,
+      age: "40"
+    },
+    {
+      title: "Super Great Haircut",
+      description: "Review Description Review Description Review Description Review Description Review Description Review Description Review Description Review Description Review Description Review Description Review Description Review Description Review Description Review Description Review Description Review Description",
+      overall_score: "4.8",
+      style: 3,
+      gender: 0,
+      age: "60"
+    },
+  ];
 
   overall_score = (userReviews) => {
     let avgScore = 0;
@@ -23,8 +59,9 @@ class StylistPage extends Component {
     return (
       <div id="StylistPage">
         <h5 id="StylistName" className="display-5">Reviews for { stylistName }:</h5>
-        <div>
-        </div>
+        <ReviewList
+          reviews={ this.reviews }
+        />
       </div>
     );
 
